bf423837d1109e25069d59deff367a161e5ad8c0
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-10-05 22:22:19 +0200

    ALmost complete

---
+++ block-grid-umbraco/wwwroot/js/form-validation.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var scrollToForm = '@(TempData["scrollToForm"] != null ? "true" : "false")';
+
+    if (scrollToForm === "true") {
+        var formSection = document.getElementById("form-section");
+        if (formSection) {
+            formSection.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+});
+
---
+++ block-grid-umbraco/wwwroot/js/javascript.js
@@ -0,0 +1,47 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', function () {
+            menu.classList.toggle('menu-open');
+        });
+    }
+
+    document.querySelectorAll('.umb-block-grid__layout-container, .umb-block-grid__layout-item, .umb-block-grid__area').forEach(function (el) {
+        el.removeAttribute('class');
+        console.log("Class removed");
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+            1600: {
+                slidesPerView: 4,
+            }
+        },
+    });
+});
